118b32eb3e51852a11eeda7c7e32bdddfde883f6
dpfls0922 <dpfls0922@naver.com>
2023-10-10 21:46:44 +0900

    refactor: run_rb, run_ra 공통 부분 통일하기

--- 42cursus/push_swap/Mandatory/run_rb_ra.c
+++ 42cursus/push_swap/Mandatory/run_rb_ra.c
@@ -6,7 +6,7 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 20:33:27 by yerilee           #+#    #+#             */
-/*   Updated: 2023/09/27 20:33:48 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/10/10 21:45:24 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	run_rb(t_stack *b, int rb_cnt)
 {
 	if (rb_cnt < 0)
 	{
-		rb_cnt = -rb_cnt;
+		rb_cnt = rb_cnt * -1;
 		while (rb_cnt--)
 			rrb(b);
 	}
