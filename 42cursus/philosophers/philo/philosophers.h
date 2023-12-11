729b87112a4177953685dfbea3a0550018e9605d
dpfls0922 <dpfls0922@naver.com>
2023-12-11 15:11:58 +0900

    [Add] 구조체 선언 및 변수 추가

--- 42cursus/philosophers/philo/philosophers.h
+++ 42cursus/philosophers/philo/philosophers.h
@@ -6,11 +6,23 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 20:57:33 by yerilee           #+#    #+#             */
-/*   Updated: 2023/12/06 16:06:54 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/12/06 16:41:16 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PILOSOPHERS_H
 # define PILOSOPHERS_H
+
+typedef struct s_philo
+{
+}	t_philo;
+
+typedef struct s_argv
+{
+	int	numbers_of_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+}	t_argv;
 
 #endif
