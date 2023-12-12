0485782a1938e87106e23abe13d8f3030ff3c33e
dpfls0922 <dpfls0922@naver.com>
2023-12-12 17:33:12 +0900

    [Add] atoi 함수 추가

---
+++ 42cursus/philosophers/philo/utils.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 14:37:44 by yerilee           #+#    #+#             */
+/*   Updated: 2023/12/12 14:39:04 by yerilee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	check_numeric(const char *str, int i)
+{
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	long	sign;
+	long	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	if (!check_numeric(str, i))
+		return (-1);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return ((int)(sign * res));
+}
