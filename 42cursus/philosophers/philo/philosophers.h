ebfac110621ac55a80dc1b5903d575b2e6b1ed5c
dpfls0922 <dpfls0922@naver.com>
2023-12-13 20:03:03 +0900

    [Chore] 변수명, 변수타입 수정 및 time 관련 라이브러리 include

--- 42cursus/philosophers/philo/philosophers.h
+++ 42cursus/philosophers/philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 20:57:33 by yerilee           #+#    #+#             */
-/*   Updated: 2023/12/13 16:16:06 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/12/13 20:02:06 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,41 +17,36 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-#define N 5
-#define THINKING 2
-#define HUNGRY 1
-#define EATING 0
-#define LEFT (phnum + 4) % N
-#define RIGHT (phnum + 1) % N
+#include <sys/time.h>
 
 typedef struct s_argv
 {
-	int		argc;
-	char	**argv;
-	int		numbers_of_philo;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		must_eat;
-	int		is_dead;
-	int		all_ate;
+	int			argc;
+	char		**argv;
+	int			numbers_of_philo;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			must_eat_cnt;
+	int			is_dead;
+	int			all_ate;
+	long long	created_time;
 
 	pthread_mutex_t	*fork;
 	pthread_mutex_t	eat;
-	pthread_mutex_t	output;
+	pthread_mutex_t	status;
 }	t_argv;
 
 typedef struct s_philo
 {
-	int			id;
-	int			right_fork;
-	int			left_fork;
-	int			eat_cnt;
-	int			last_meal;
-	int			last_time;
-	pthread_t	thread_id;
-	t_argv		*digning;
+	int				id;
+	int				eat_cnt;
+	int				last_meal;
+	int				last_time;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	*left_fork;
+	pthread_t		thread_id;
+	t_argv			*digning;
 }	t_philo; 
 
 int	check_numeric(const char *str, int i);
