fd7c0e0f5b1540962439b36dcb4b5c87af098892
dpfls0922 <dpfls0922@naver.com>
2023-12-15 15:35:07 +0900

    [Feat] 철학자가 죽었는지 확인하는 함수 및 쓰레드 정리하는 함수 구현

--- 42cursus/philosophers/philo/main.c
+++ 42cursus/philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 15:56:57 by yerilee           #+#    #+#             */
-/*   Updated: 2023/12/15 15:32:58 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/12/15 15:34:13 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,77 @@ void	*thread_routine(void *ptr)
 		print_status(philosopher->digning, philosopher->id, "is thinking");
 	}
 	return (NULL);
+}
+
+int	check_total_eat(t_argv *digning)
+{
+	if (digning->argc == 6)
+	{
+		if (digning->total_eat_cnt >= digning->numbers_of_philo * digning->must_eat_cnt)
+			return (1);
+	}
+	return (0);
+}
+
+int	check_dead(t_argv *digining, t_philo *philo)
+{
+	int			i;
+	long long	curent_time;
+
+	i = 1;
+	while (i <= digining->numbers_of_philo)
+	{
+		if (check_total_eat(digining))
+			return (1);
+		curent_time = get_timestamp();
+		if (curent_time - philo[i - 1].last_meal >= digining->time_to_die)
+		{
+			digining->is_dead = 1;
+			// printf로 메세지 출력하는 것으로 바꿀 것
+			print_status(philo->digning, philo->id, "died");
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	mutex_destroy(t_argv *digning, t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	pthread_mutex_destroy(&digning->status);
+	pthread_mutex_destroy(&digning->eat_cnt);
+	while (i < digning->numbers_of_philo)
+	{
+		pthread_detach(philo[i].thread_id);
+		pthread_mutex_destroy(philo[i].right_fork);
+		pthread_mutex_destroy(philo[i].left_fork);
+		i++;
+	}
+}
+
+int ft_join_destroy(t_argv *digning, t_philo *philo) {
+	int	i;
+
+	if (check_dead(digning, philo))
+	{
+		mutex_destroy(digning, philo);
+		free(digning);
+		return (0);
+	}
+	i = 0;
+	 while (i < digning->numbers_of_philo)
+	{
+		if (pthread_join(philo[i].thread_id, NULL) != 0)
+		{
+			 free(digning);
+			return (0);
+		}
+		 i++;
+	}
+	return (1);
 }
 
 int	ft_create_philo(t_argv *digning, t_philo **philo)
