3456717ea9e5a56bdda62754077eef2700d1c4e3
dpfls0922 <dpfls0922@naver.com>
2023-12-21 19:17:08 +0900

    [Refactor] 필로소퍼 쓰레드 생성 관련 함수 파일 분리

---
+++ 42cursus/philosophers/philo/create_phio.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_phio.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 19:16:16 by yerilee           #+#    #+#             */
+/*   Updated: 2023/12/21 19:16:30 by yerilee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	ft_create_philo(t_argv *digning)
+{
+	int		i;
+	t_philo *p;
+
+	i = 0;
+	digning->created_time = get_timestamp();
+	while (i < digning->numbers_of_philo)
+	{
+		digning->philo[i].last_meal = get_timestamp();
+		p = &digning->philo[i];
+		if (pthread_create(&p->thread_id, NULL, &thread_routine, p) != 0)
+		{
+			free(digning);
+			return (0);
+		}
+		usleep(100);
+		i++;
+	}
+	if (!ft_join_destroy(digning))
+		return (0);
+	return (1);
+}
