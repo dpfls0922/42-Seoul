341e0b3989a7b96f6e5124e738828cbafea5f413
dpfls0922 <dpfls0922@naver.com>
2023-12-21 19:02:27 +0900

    [Chore] 뮤텍스 초기화하는 함수명 변경

--- 42cursus/philosophers/philo/main.c
+++ 42cursus/philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 16:33:33 by yerilee           #+#    #+#             */
-/*   Updated: 2023/12/21 19:00:09 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/12/21 19:02:07 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -179,7 +179,7 @@ int	ft_create_philo(t_argv *digning)
 	return (1);
 }
 
-int	ft_start_mutex(t_argv *digning)
+int	ft_init_mutex(t_argv *digning)
 {
 	int	i;
 
@@ -258,10 +258,10 @@ int	main(int argc, char **argv)
 		return (-1);
 	// input error check
 	digning->argc = argc;
-	digning->argv=argv;
+	digning->argv = argv;
 	if(!ft_init_data(digning))
 		return (1);
-	if(!ft_start_mutex(digning))
+	if(!ft_init_mutex(digning))
 		return (1);
 	ft_init_philo(digning);
 	if(!ft_create_philo(digning))
