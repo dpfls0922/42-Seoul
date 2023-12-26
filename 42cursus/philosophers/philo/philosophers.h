d8c139450ef72cfd8a1d0fae7fb3bea27054a25c
dpfls0922 <dpfls0922@naver.com>
2023-12-26 22:30:40 +0900

    [Chore] 사용하지 않는 함수 삭제 및 선언되지 않은 함수 추가

--- 42cursus/philosophers/philo/philosophers.h
+++ 42cursus/philosophers/philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 20:57:33 by yerilee           #+#    #+#             */
-/*   Updated: 2023/12/26 19:59:46 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/12/26 22:26:34 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,6 @@ int			ft_init_data(t_argv *digning);
 
 int			ft_create_philo(t_argv *digning);
 
-void		grabbing_fork(t_philo *philosopher);
 void		eating(t_philo *philosopher);
 void		sleeping(t_argv *digning, long long time_to_sleep);
 void		print_status(t_argv *digning, int philo_id, char *status);
@@ -67,7 +66,7 @@ void		*thread_routine(void *ptr);
 int			check_total_eat(t_argv *digning);
 int			check_finish(t_argv *digning);
 
-int			ft_create_philo(t_argv *digning);
+void		mutex_destroy(t_argv *digning, t_philo *philo);
 int			ft_join_destroy(t_argv *digning);
 
 #endif
