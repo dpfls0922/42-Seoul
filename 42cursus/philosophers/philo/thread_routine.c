b043fd4cfba6f5dd321294622f707f44ea151c02
dpfls0922 <dpfls0922@naver.com>
2023-12-26 22:30:11 +0900

    [Chore] 철학자가 1일 경우 예외 처리 추가 및 홀수 번째 철학자가 먼저 실행하도록 수정

--- 42cursus/philosophers/philo/thread_routine.c
+++ 42cursus/philosophers/philo/thread_routine.c
@@ -6,31 +6,30 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 19:09:21 by yerilee           #+#    #+#             */
-/*   Updated: 2023/12/26 16:57:41 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/12/26 22:15:40 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	grabbing_fork(t_philo *philosopher)
-{
-	pthread_mutex_lock(philosopher->right_fork);
-	print_status(philosopher->digning, philosopher->id, "has taken a fork");
-	pthread_mutex_lock(philosopher->left_fork);
-	print_status(philosopher->digning, philosopher->id, "has taken a fork");
-}
-
 void	eating(t_philo *philosopher)
 {
-	grabbing_fork(philosopher);
-	print_status(philosopher->digning, philosopher->id, "is eating");
-	philosopher->last_meal = get_timestamp();
-	sleeping(philosopher->digning, philosopher->digning->time_to_eat);
-	philosopher->digning->total_eat_cnt++;
-	pthread_mutex_lock(&philosopher->digning->eat_cnt);
-	philosopher->eat_cnt++;
-	pthread_mutex_unlock(&philosopher->digning->eat_cnt);
-	pthread_mutex_unlock(philosopher->right_fork);
+	pthread_mutex_lock(philosopher->left_fork);
+	print_status(philosopher->digning, philosopher->id, "has taken a fork");
+	if (philosopher->digning->numbers_of_philo != 1)
+	{
+		pthread_mutex_lock(philosopher->right_fork);
+		print_status(philosopher->digning, philosopher->id, "has taken a fork");
+		print_status(philosopher->digning, philosopher->id, "is eating");
+		philosopher->last_meal = get_timestamp();
+		sleeping(philosopher->digning, philosopher->digning->time_to_eat);
+		pthread_mutex_lock(&philosopher->digning->eat_cnt);
+		philosopher->eat_cnt++;
+		pthread_mutex_unlock(&philosopher->digning->eat_cnt);
+		pthread_mutex_unlock(philosopher->right_fork);
+		if (philosopher->digning->must_eat_cnt == philosopher->eat_cnt)
+			philosopher->digning->total_eat_cnt++;
+	}
 	pthread_mutex_unlock(philosopher->left_fork);
 }
 
@@ -53,23 +52,28 @@ void	print_status(t_argv *digning, int philo_id, char *status)
 	if (!(digning->is_dead))
 	{
 		printf("%lld ", get_timestamp() - digning->created_time);
-		printf("%d %s\n", philo_id, status);
+		printf("%d %s\n", philo_id + 1, status);
 	}
 	pthread_mutex_unlock(&digning->status);
 }
 
 void	*thread_routine(void *ptr)
 {
+	t_argv	*digning;
 	t_philo	*philosopher;
 
 	philosopher = (t_philo *)ptr;
-	while (!(philosopher->digning->is_dead)
-		&& (philosopher->eat_cnt != philosopher->digning->must_eat_cnt))
+	digning = philosopher->digning;
+	if (philosopher->id % 2 == 0)
+		usleep(100);
+	while (!(digning->is_dead) && digning->must_eat_cnt != philosopher->eat_cnt)
 	{
 		eating(philosopher);
-		print_status(philosopher->digning, philosopher->id, "is sleeping");
-		sleeping(philosopher->digning, philosopher->digning->time_to_sleep);
-		print_status(philosopher->digning, philosopher->id, "is thinking");
+		if (digning->numbers_of_philo == 1)
+			sleeping(digning, digning->time_to_sleep);
+		print_status(digning, philosopher->id, "is sleeping");
+		sleeping(digning, digning->time_to_sleep);
+		print_status(digning, philosopher->id, "is thinking");
 	}
 	return (NULL);
 }
