a42be8c826e04eb38426b8c35cc26fb30f503209
dpfls0922 <dpfls0922@naver.com>
2023-12-27 14:47:25 +0900

    [Fix] 호출할 함수에게 매개변수 전달

--- 42cursus/philosophers/philo/check.c
+++ 42cursus/philosophers/philo/check.c
@@ -6,7 +6,7 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 19:08:01 by yerilee           #+#    #+#             */
-/*   Updated: 2023/12/26 22:27:28 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/12/27 14:46:58 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	check_finish(t_argv *digning)
 
 	while (!digning->is_dead)
 	{
-		if (check_total_eat)
+		if (check_total_eat(digning))
 			break ;
 		i = 0;
 		while (i < digning->numbers_of_philo)
