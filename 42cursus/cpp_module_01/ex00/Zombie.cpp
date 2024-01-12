be3c9c13cb5ed9962dba97eeddb48fc387306963
dpfls0922 <dpfls0922@naver.com>
2024-01-12 19:46:48 +0900

    [Chore] 띄어쓰기 추가

--- 42cursus/cpp_module_01/ex00/Zombie.cpp
+++ 42cursus/cpp_module_01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 17:36:09 by yerilee           #+#    #+#             */
-/*   Updated: 2024/01/12 18:44:57 by yerilee          ###   ########.fr       */
+/*   Updated: 2024/01/12 19:46:38 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ Zombie::Zombie(std::string name)
 
 Zombie::~Zombie()
 {
-    std::cout << this->name << " :";
+    std::cout << this->name << " : ";
     std::cout << "is dead\n";
 }
 
 void Zombie::announce()
 {
-    std::cout << this->name << " :";
+    std::cout << this->name << " : ";
     std::cout << "BraiiiiiiinnnzzzZ...\n";
 }
