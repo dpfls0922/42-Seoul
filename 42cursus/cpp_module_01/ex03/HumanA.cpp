411194d2f5faf3e91aad2765db473a2d061cd199
dpfls0922 <dpfls0922@naver.com>
2024-01-14 13:36:22 +0900

    [Feat] HumanA 클래스 멤버 함수 구현

---
+++ 42cursus/cpp_module_01/ex03/HumanA.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 13:28:54 by yerilee           #+#    #+#             */
+/*   Updated: 2024/01/14 13:35:25 by yerilee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanA.hpp"
+
+HumanA::HumanA(std::string name, Weapon &weapon): name(name), weapon(weapon)
+{
+}
+
+HumanA::~HumanA()
+{
+}
+
+void HumanA::attack()
+{
+    std::cout << this->name << "attacks with their ";
+    std::cout << this->weapon.getType() <<"\n";
+}
