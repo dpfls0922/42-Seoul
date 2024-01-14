87e8f4afd59d0d7e722651dba7be1817d6cfc18a
dpfls0922 <dpfls0922@naver.com>
2024-01-14 11:06:11 +0900

    [Del] 불필요한 코드 제거

--- 42cursus/cpp_module_01/ex01/Zombie.cpp
+++ 42cursus/cpp_module_01/ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 17:36:09 by yerilee           #+#    #+#             */
-/*   Updated: 2024/01/12 22:15:09 by yerilee          ###   ########.fr       */
+/*   Updated: 2024/01/14 11:05:24 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,5 @@ void Zombie::announce()
 
 void Zombie::setName(std::string name)
 {
-    if (name == "")
-        return;
     this->name = name;
 }
