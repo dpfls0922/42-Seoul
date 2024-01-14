de34f7ede3b7383015fc04575aa4dc4551d8187f
dpfls0922 <dpfls0922@naver.com>
2024-01-14 13:47:16 +0900

    [Feat] HumanB 클래스 멤버 함수 정의

---
+++ 42cursus/cpp_module_01/ex03/HumanB.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 13:39:32 by yerilee           #+#    #+#             */
+/*   Updated: 2024/01/14 13:46:34 by yerilee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name)
+{
+    this->name = name;
+    this->weapon = NULL;
+}
+
+HumanB::~HumanB()
+{
+}
+
+void HumanB::attack()
+{
+    if (weapon)
+    {
+        std::cout << this->name << "attacks with their ";
+        std::cout << this->weapon->getType() <<"\n";
+    }
+}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+    this->weapon = &weapon;
+}
