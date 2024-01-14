e8650fa87292c6cbd6d686266a6b0989acfa34d8
dpfls0922 <dpfls0922@naver.com>
2024-01-14 13:47:02 +0900

    [Feat] HumanB 클래스 정의

---
+++ 42cursus/cpp_module_01/ex03/HumanB.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 12:07:12 by yerilee           #+#    #+#             */
+/*   Updated: 2024/01/14 13:42:19 by yerilee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+
+#include <iostream>
+#include "Weapon.hpp"
+
+class HumanB{
+    private:
+        std::string name;
+        Weapon *weapon;
+    public:
+        HumanB(std::string name);
+        ~HumanB();
+        void attack();
+        void setWeapon(Weapon &weapon);
+};
+
+#endif
