36123abaa9207c59afb7a664b86d14b71a2bf1d0
dpfls0922 <dpfls0922@naver.com>
2024-01-14 16:39:46 +0900

    [Feat] complain 함수 호출 예제

---
+++ 42cursus/cpp_module_01/ex05/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 15:21:05 by yerilee           #+#    #+#             */
+/*   Updated: 2024/01/14 16:11:47 by yerilee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+}
