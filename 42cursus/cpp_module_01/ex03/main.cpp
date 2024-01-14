d60a488695ade8b4a146eaacdf1fbdf3e1d0cc70
dpfls0922 <dpfls0922@naver.com>
2024-01-14 13:08:29 +0900

    [Add] 레퍼런스와 포인터를 이용함 예제

---
+++ 42cursus/cpp_module_01/ex03/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 13:06:44 by yerilee           #+#    #+#             */
+/*   Updated: 2024/01/14 13:07:00 by yerilee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
