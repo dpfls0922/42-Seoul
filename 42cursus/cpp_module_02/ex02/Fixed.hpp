e1504dd79ac0243220182f7653dd184778155e23
dpfls0922 <dpfls0922@naver.com>
2024-01-23 16:30:07 +0900

    [Add] 연산자 오버로딩 함수 클래스에 추가

---
+++ 42cursus/cpp_module_02/ex02/Fixed.hpp
@@ -0,0 +1,46 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed{
+    private:
+        int                 num;
+        static const int    bits = 8;
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(const int num);
+        Fixed(const float num);
+        Fixed(const Fixed &obj);
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+        float   toFloat( void ) const;
+        int     toInt( void ) const;
+
+        bool operator>(const Fixed &obj) const;
+        bool operator<(const Fixed &obj) const;
+        bool operator>=(const Fixed &obj) const;
+        bool operator<=(const Fixed &obj) const;
+        bool operator==(const Fixed &obj) const;
+        bool operator!=(const Fixed &obj) const;
+
+        Fixed& operator=(const Fixed &obj);
+        Fixed operator+(const Fixed &obj) const;
+        Fixed operator-(const Fixed &obj) const;
+        Fixed operator/(const Fixed &obj) const;
+        Fixed operator*(const Fixed &obj) const;
+
+        Fixed& operator++(void);
+        Fixed& operator--(void);
+        const Fixed operator++(int);
+        const Fixed operator--(int);
+
+        static Fixed &min(Fixed &fix1, Fixed &fix2);
+        static const Fixed &min(const Fixed &fix1, const Fixed &fix2);
+        static Fixed &max(Fixed &fix1, Fixed &fix2);
+        static const Fixed &max(const Fixed &fix1, const Fixed &fix2);
+        ~Fixed();
+};
+std::ostream &operator<<(std::ostream &os, const Fixed &obj);
+#endif
