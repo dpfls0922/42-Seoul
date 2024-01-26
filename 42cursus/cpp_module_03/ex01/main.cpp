1f26df51a5d6cccf36ad0a3e68d745a677f4fddc
dpfls0922 <dpfls0922@naver.com>
2024-01-26 15:29:36 +0900

    [Add] virtual 키워드가 필요한 예제 추가

--- 42cursus/cpp_module_03/ex01/main.cpp
+++ 42cursus/cpp_module_03/ex01/main.cpp
@@ -3,9 +3,13 @@
 
 int	main(void)
 {
+	ClapTrap *parent;
+	parent = new ScavTrap("child");
+
 	ScavTrap a("A");
 	ScavTrap b("B");
 	a.attack("B");
+	parent->attack("B");
 	b.takeDamage(10);
 	b.beRepaired(2);
 	a.guardGate();
