7dbcf75ba9849d7e0a552647df953fd666044df7
dpfls0922 <dpfls0922@naver.com>
2024-01-26 19:33:19 +0900

    [Add] Brain 포인터와 관련 멤버함수 추가

--- 42cursus/cpp_module_04/ex01/Cat.hpp
+++ 42cursus/cpp_module_04/ex01/Cat.hpp
@@ -2,14 +2,19 @@
 # define CAT_HPP
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Cat : public Animal {
+    private:
+        Brain *brain;
     public:
         Cat();
         Cat(const Cat &obj);
         Cat& operator=(const Cat& obj);
-        ~Cat(void);
+        ~Cat();
 
+        std::string getCatIdea(int index);
+        void setCatIdea(int index, std::string str);
         void makeSound(void) const;
 };
 
