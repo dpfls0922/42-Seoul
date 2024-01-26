985162ce4d68847238d55e4d9ec7073251c1a5f7
dpfls0922 <dpfls0922@naver.com>
2024-01-26 19:37:35 +0900

    [Feat] 생성 및 소멸 시 Brain 객체도 같이 생성 및 소멸하도록 함

--- 42cursus/cpp_module_04/ex01/Dog.cpp
+++ 42cursus/cpp_module_04/ex01/Dog.cpp
@@ -1,14 +1,17 @@
 
 #include "Dog.hpp"
+#include "Brain.hpp"
 
 Dog::Dog()
 {
 	this->type = "Dog";
+	this->brain = new Brain();
 	std::cout << "Dog default constructor called\n";
 }
 
 Dog::Dog(const Dog &obj)
 {
+	this->brain = new Brain();
 	*this = obj;
 	std::cout << "Dog copy constructor called\n";
 }
@@ -18,6 +21,11 @@ Dog& Dog::operator=(const Dog& obj)
 	if (this != &obj)
 	{
 		this->type = obj.getType();
+		if (this->brain)
+		{
+			delete this->brain;
+		}
+		this->brain = new Brain(*obj.brain);
 		std::cout << "Dog operator = " << type << " called\n";
 	}
 	return (*this);
@@ -25,7 +33,28 @@ Dog& Dog::operator=(const Dog& obj)
 
 Dog::~Dog(void)
 {
+	delete brain;
 	std::cout << "Dog Destructor called\n";
+}
+
+std::string Dog::getDogIdea(int index)
+{	
+	if (index >= 100)
+	{
+		std::cout << "Error: Out of range\n";
+		return "";
+	}
+	return this->brain->getIdea(index);
+}
+
+void Dog::setDogIdea(int index, std::string str)
+{
+	if (index >= 100)
+	{
+		std::cout << "Error: Out of range\n";
+		return ;
+	}
+	this->brain->setIdea(index, str);
 }
 
 void Dog::makeSound(void) const
