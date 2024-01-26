711339691e60d301a3ef569fa71f8b7a27096945
dpfls0922 <dpfls0922@naver.com>
2024-01-26 19:37:17 +0900

    [Add] Brain 포인터와 관련 멤버함수 추가

--- 42cursus/cpp_module_04/ex01/Dog.hpp
+++ 42cursus/cpp_module_04/ex01/Dog.hpp
@@ -4,12 +4,16 @@
 #include "Animal.hpp"
 
 class Dog : public Animal {
+    private:
+        Brain *brain;
     public:
         Dog();
         Dog(const Dog &dog);
         Dog& operator=(const Dog &dog);
         ~Dog();
 
+        std::string getDogIdea(int index);
+        void setDogIdea(int index, std::string str);
         void makeSound(void) const;
 };
 
